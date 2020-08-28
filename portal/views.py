6b80a15a4b665fa18523b2a11b51c9229a991e3b
Babalola Peter <ayodelebabalola1004@gmail.com>
2020-08-28 20:54:44 +0100

    updated few files

--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,7 @@
 import csv
 import io
 import weasyprint
+from itertools import islice
 
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth import update_session_auth_hash, get_user_model
@@ -1032,16 +1033,18 @@ def add_score_for(request, id):
         csv_file = request.FILES['file']
         data_set = csv_file.read().decode('UTF-8')
         io_string = io.StringIO(data_set)
-        next(io_string)
+        for skip in range(11):
+            next(io_string)
         for column in csv.reader(io_string, delimiter=',', quotechar="|"):
-            ids = ids + (column[0],)
-            cas = cas + (column[1],)
-            exams = exams + (column[2],)
-
+            ids = ids + (column[2],)
+            cas = cas + (column[9],)
+            exams = exams + (column[16],)
         for s in range(0, len(ids)):
-            student = TakenCourse.objects.get(
-                student__id_number=ids[s], course__id=id, )
-            # nursing = Student.objects.get(department='NURSING')
+            try:
+                student = TakenCourse.objects.get(
+                    student__id_number=ids[s], course__id=id)
+            except:
+                continue
             courses = TakenCourse.objects.filter(
                 student__id_number=ids[s], course__semester=current_semester)
             total_unit_in_semester = 0
@@ -1099,40 +1102,41 @@ def scoresheet_download(request, id):
 
     current_semester = Semester.objects.get(is_current_semester=True)
     course = TakenCourse.objects.filter(course__id=id)
-    # title = [['ADELEKE UNIVERSITY, EDE'],
-    #          [f'{current_semester.session} ACADEMIC SESSION'],
-    #          [f'{current_semester} Semester'],
-    #          [f'{course.course.courseTitle} ({course.course.courseCode} - {course.course.courseUnit}Unit)'],
-    #          [f'{course.course.level} Level']
-    #         ]
+
     header = [['ADELEKE UNIVERSITY, EDE'],
-             [f'{current_semester.session} ACADEMIC SESSION'],
-             [f'{current_semester} Semester'],
-             [f'{i.course.courseTitle} ({i.course.courseCode} - {i.course.courseUnit}Unit)' for i in course],
-             [f'{i.course.level} Level' for i in course]
-             ]
+              [f'{current_semester.session} ACADEMIC SESSION'],
+              [f'{current_semester} Semester'],
+              [f'{i.course.courseTitle} ({i.course.courseCode} - {i.course.courseUnit}Unit)' for i in course],
+              [f'{i.course.level} Level' for i in course]
+              ]
 
     content = [['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-              ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-              ['', '', '', '', '', 'Break Down', '', '', '', '', '', '', '', '', '', '', ''],
-              ['', '', '', 'Summary', '', 'Continous Assessment', '',
-                  '', '', '', 'Examination', '', '', '', '', '', ''],
-              ['', '', '', 'Score', '', 'Att.', 'ASSIGN.', 'Quiz', 'Test',
-                  'Total', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Total'],
-              ['S/N', 'NAMES', 'MATRIC NO', '100', 'Grade', '5', '10', '10',
-                  '15', '40', '15', '15', '15', '15', '15', '15', '60']
-              ]
-    
-    alc = CourseAllocation.objects.get(courses__allocated_course__lecturer__pk=request.user.id, courses__pk=id)
+               ['', '', '', '', '', '', '', '', '',
+                   '', '', '', '', '', '', '', ''],
+               ['', '', '', '', '', 'Break Down', '', '',
+                   '', '', '', '', '', '', '', '', ''],
+               ['', '', '', 'Summary', '', 'Continous Assessment', '',
+                '', '', '', 'Examination', '', '', '', '', '', ''],
+               ['', '', '', 'Score', '', 'Att.', 'ASSIGN.', 'Quiz', 'Test',
+                'Total', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Total'],
+               ['S/N', 'NAMES', 'MATRIC NO', '100', 'Grade', '5', '10', '10',
+                '15', '40', '15', '15', '15', '15', '15', '15', '60']
+               ]
+
+    alc = CourseAllocation.objects.get(
+        courses__allocated_course__lecturer__pk=request.user.id, courses__pk=id)
     footer = [['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
               ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-              [f'Lecturer\'s Name: {alc.lecturer.get_full_name()}', '', 'HOD\'s Name: ................................', '', 'Dean\'s NAME: ................................', '' '', '', '', '', '', '', '', '', '', '', ''],
+              [f'Lecturer\'s Name: {alc.lecturer.get_full_name()}', '', 'HOD\'s Name: ................................',
+               '', 'Dean\'s NAME: ................................', '' '', '', '', '', '', '', '', '', '', '', ''],
               ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-              ['Signature: ................', '', 'Signature: ................', '', 'Signature: ................', '', '', '', '', '', '', '', '', '', '', '', '']
-             ] 
+              ['Signature: ................', '', 'Signature: ................', '',
+                  'Signature: ................', '', '', '', '', '', '', '', '', '', '', '', '']
+              ]
     sn = 1
     for i in course:
-        content.append([sn, i.student.user.get_full_name(), i.student.id_number, '', '', '', '', '', '', '', '', '', '', '', '', '', ''])
+        content.append([sn, i.student.user.get_full_name(
+        ), i.student.id_number, '', '', '', '', '', '', '', '', '', '', '', '', '', ''])
         sn += 1
     writer.writerows(header)
     writer.writerows(content)
