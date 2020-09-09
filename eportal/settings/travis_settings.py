5bc92b56243a29861e61a1c012c9276fdb017875
Babalola Peter <ayodelebabalola1004@gmail.com>
2020-09-09 13:05:35 +0100

    travis ci

---
+++ eportal/settings/travis_settings.py
@@ -0,0 +1,16 @@
+from . base import *
+# Use the following live settings to build on Travis CI
+if os.getenv('BUILD_ON_TRAVIS', None):
+    SECRET_KEY = config("SECRET_KEY")
+    DEBUG = False
+    TEMPLATE_DEBUG = True
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'travis_ci_db',
+            'USER': 'travis',
+            'PASSWORD': '',
+            'HOST': '127.0.0.1',
+        }
+    }
