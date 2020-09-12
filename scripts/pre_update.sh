9a714d4672f2f7284018e1c90ad8594e9a6b7aeb
Babalola Peter <ayodelebabalola1004@gmail.com>
2020-09-12 10:47:38 +0100

    update scripts

--- scripts/pre_update.sh
+++ scripts/pre_update.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-command cd /home/ubuntu/eportal
-command rm !(".env"|"env")
+###########################################################
+# Author: Peter Babalola
+# Version: 0.0
+# Date: 2020-09-12
+# Description: script to clean my project dir on the server
+# Usage: ./pre_update.sh
+###########################################################
+
+command mv /home/ubuntu/eportal/env /home/ubuntu/env
+command mv /home/ubuntu/eportal/.env /home/ubuntu/.env
+command rm -rf -v /home/ubuntu/eportal/*
+command mv /home/ubuntu/env /home/ubuntu/env
+command mv /home/ubuntu/.env /home/ubuntu/.env
+command echo "done"
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-command service supervisorctl stop all
+# command service supervisorctl stop all
+mv /home/ayo/Repos/eportal/config /home/ayo/Repos/config
+mv /home/ayo/Repos/eportal/.git /home/ayo/Repos/.git
+mv /home/ayo/Repos/eportal/.env /home/ayo/Repos/.env
+rm -rf /home/ayo/Repos/eportal/*
+rm -rf /home/ayo/Repos/eportal/.*
+mv /home/ayo/Repos/config /home/ayo/Repos/eportal/config
+mv /home/ayo/Repos/.env /home/ayo/Repos/eportal/.env
+mv /home/ayo/Repos/.git /home/ayo/Repos/eportal/.git
+echo "done"
