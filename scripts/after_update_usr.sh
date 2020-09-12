d48283d45b9c473f74ca279b536b626892edfa6f
Babalola Peter <ayodelebabalola1004@gmail.com>
2020-09-12 11:08:41 +0100

    update scripts

--- scripts/after_update_usr.sh
+++ scripts/after_update_usr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 command /home/ubuntu/eportal/env/bin/activate
-command pip install -r /home/ubuntu/eportal/requirements.txt
-command python manage.py makemigrations --settings=eportal.settings.pro
-command python manage.py migrate --settings=eportal.settings.pro
+command pip3 install -r /home/ubuntu/eportal/requirements.txt
+command python3 manage.py makemigrations --settings=eportal.settings.pro
+command python3 manage.py migrate --settings=eportal.settings.pro
