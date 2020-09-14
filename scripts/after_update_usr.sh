4b0c2faebf8adddb99fe37bd6bdb070783dc54fa
Babalola Peter <ayodelebabalola1004@gmail.com>
2020-09-14 05:30:07 +0100

    fixed minor issues

--- scripts/after_update_usr.sh
+++ scripts/after_update_usr.sh
@@ -11,7 +11,6 @@
 command source /home/ubuntu/eportal/env/bin/activate
 command pip3 install -r /home/ubuntu/eportal/requirements.txt
 command cd /home/ubuntu/eportal
-command export DJANGO_SETTIINGS_MODULE=eportal.settings.pro
-command python3 manage.py makemigrations
-command python3 manage.py migrate
-command python3 manage.py collectstatic
+command ./manage.py makemigrations --settings=eportal.settings.pro
+command ./manage.py migrate --settings=eportal.settings.pro
+command ./manage.py collectstatic --settings=eportal.settings.pro
