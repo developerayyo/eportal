fcd483761da348f7be52d475bf3dff43e2520e6b
Peter Babalola <babalolapeter@icloud.com>
2020-10-09 02:10:19 +0100

    fixed api/datatables issues

--- portal/api/views.py
+++ portal/api/views.py
@@ -19,32 +19,32 @@ from django.contrib.auth.decorators import login_required
 from ..decorators import admin_required
 
 
-@method_decorator([login_required, admin_required], name='dispatch')
+# @method_decorator([login_required, admin_required], name='dispatch')
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-@method_decorator([login_required, admin_required], name='dispatch')
+# @method_decorator([login_required, admin_required], name='dispatch')
 class LecturerViewSet(viewsets.ModelViewSet):
     queryset = User.objects.filter(is_lecturer=True)
     serializer_class = UserSerializer
 
-@method_decorator([login_required, admin_required], name='dispatch')
+# @method_decorator([login_required, admin_required], name='dispatch')
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-@method_decorator([login_required, admin_required], name='dispatch')
+# @method_decorator([login_required, admin_required], name='dispatch')
 class CourseAllocationViewSet(viewsets.ModelViewSet):
     queryset = CourseAllocation.objects.all()
     serializer_class = CourseAllocationSerializer
 
-@method_decorator([login_required, admin_required], name='dispatch')
+# @method_decorator([login_required, admin_required], name='dispatch')
 class SessionViewSet(viewsets.ModelViewSet):
     queryset = Session.objects.all().order_by('-session')
     serializer_class = SessionSerializer
 
-@method_decorator([login_required, admin_required], name='dispatch')
+# @method_decorator([login_required, admin_required], name='dispatch')
 class SemesterViewSet(viewsets.ModelViewSet):
     queryset = Semester.objects.all().order_by('-session')
     serializer_class = SemesterSerializer
