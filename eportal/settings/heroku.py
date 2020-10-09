f869de33f0c07a11f49a84ec36cdd944418efd08
Peter Babalola <babalolapeter@icloud.com>
2020-10-09 17:05:40 +0100

    added whitenoise

--- eportal/settings/heroku.py
+++ eportal/settings/heroku.py
@@ -43,15 +43,5 @@ CELERY_TASK_SERIALIZER='json'
 CELERY_RESULT_SERIALIZER='json'
 CELERY_TIMEZONE='Africa/Nairobi'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'portal/static'),
-)
 # Activate Django-Heroku.
 django_heroku.settings(locals())
